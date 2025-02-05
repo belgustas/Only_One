c2d492971db15bb073415718a8de71480cfc8d7d
Rinde-beep <9heartosu@gmail.com>
2025-02-05 21:13:06 +0300

    Add files via upload

---
+++ begining.py
@@ -0,0 +1,74 @@
+import pygame
+import sys
+import os
+from main import main
+
+pygame.init()
+
+WIDTH, HEIGHT = 650, 650
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Begining")
+background = pygame.image.load("backgroundfor.jpg").convert()
+background = pygame.transform.scale(background, (750, 750))
+all_sprites = pygame.sprite.Group()
+
+running = True
+clock = pygame.time.Clock()
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class But(pygame.sprite.Sprite):
+    def __init__(self, WIDTH, HEIGHT, low, name):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image(name)
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH
+        self.rect.y = HEIGHT
+        self.low = low
+
+    def clicked(self, x, y):
+        print(x, y)
+        print(self.rect.x, self.rect.y)
+        if ((x >= self.rect.x) and (x <= self.rect.x + self.low)) and ((y >= self.rect.y) and (y <= self.rect.y + self.low)):
+            main()
+
+but1 = But(WIDTH // 3 - 50, HEIGHT // 2 - 35, 70, "settings.png")
+but2 = But(WIDTH // 2 - 50, HEIGHT // 2 - 50, 100, "Play.png")
+but3 = But(WIDTH // 3 * 2 - 20, HEIGHT // 2 - 35, 70, "leaders.png")
+all_sprites.add(but1, but2, but3)
+
+while running:
+    screen.blit(background, (-50, -50))
+
+    # Обработчик событий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            but1.clicked(mouse_x, mouse_y)
+            but2.clicked(mouse_x, mouse_y)
+            but3.clicked(mouse_x, mouse_y)
+
+
+    all_sprites.update()
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(60)  # FPS
+
+pygame.quit()
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,48 +3,50 @@ from player import Player  # Импортируем класс игрока
 from enemy import Enemy  # Импортируем класс врага
 from bowbar import BowBar
 
-pygame.init()
+def main():
+    pygame.init()
 
-# Настройки окна
-WIDTH, HEIGHT = 650, 650
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Only one!")
-background = pygame.image.load("img/arena_img.png").convert()
-# Создание группы спрайтов
-all_sprites = pygame.sprite.Group()
+    # Настройки окна
+    WIDTH, HEIGHT = 650, 650
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Only one!")
+    background = pygame.image.load("img/arena_img.png").convert()
+    # Создание группы спрайтов
+    all_sprites = pygame.sprite.Group()
 
-player = Player(WIDTH // 2, HEIGHT // 2, all_sprites)
-enemy = Enemy(100, 100, player)
-all_sprites.add(player, enemy)
-running = True
-clock = pygame.time.Clock()
-bow = BowBar(0)
-all_sprites.add(bow)
+    player = Player(WIDTH // 2, HEIGHT // 2, all_sprites)
+    enemy = Enemy(100, 100, player)
+    all_sprites.add(player, enemy)
+    running = True
+    clock = pygame.time.Clock()
+    bow = BowBar(0)
+    all_sprites.add(bow)
 
-while running:
-    screen.blit(background, (0, 0))
+    while running:
+        screen.blit(background, (0, 0))
 
-    # Обработчик событий
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                bow.update()
-                if bow.rect.x == 150:
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    player.shoot(mouse_x, mouse_y)
-                bow.count = 0
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bow.update()
-                bow.count = 1
+        # Обработчик событий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    bow.update()
+                    if bow.rect.x == 150:
+                        mouse_x, mouse_y = pygame.mouse.get_pos()
+                        player.shoot(mouse_x, mouse_y)
+                    bow.count = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bow.update()
+                    bow.count = 1
 
-    # Обновление всех спрайтов
-    all_sprites.update()
-    all_sprites.draw(screen)
 
-    pygame.display.flip()
-    clock.tick(60)  # FPS
+        # Обновление всех спрайтов
+        all_sprites.update()
+        all_sprites.draw(screen)
 
-pygame.quit()
+        pygame.display.flip()
+        clock.tick(60)  # FPS
+
+    pygame.quit()
