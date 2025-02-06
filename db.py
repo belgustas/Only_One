aa9b29f1d6e031e25673770444f90b650b857fc9
voakman <163044746+voakman@users.noreply.github.com>
2025-02-07 00:25:38 +0300

    база данных с данными и обновленный db-файл

--- db.py
+++ db.py
@@ -4,13 +4,16 @@ con = sqlite3.connect('pygame_top')
 
 cur = con.cursor()
 
-number = cur.execute("""SELECT id from top_list""").fetchall()
+def leadtable():
+    number = cur.execute("""SELECT id FROM top_list""").fetchall()
 
-nickname = cur.execute("""SELECT name from top_list""").fetchall()
+    nickname = cur.execute("""SELECT name FROM top_list""").fetchall()
 
-highest_res = cur.execute("""SELECT top_res from top_list""").fetchall()
+    highest_res = cur.execute("""SELECT top_res FROM top_list""").fetchall()
 
-table_leaders = []
+    table_leaders = []
 
-for i in range(len(highest_res)):
-    table_leaders.append(f"{number[i]} - {nickname[i]} - {highest_res[i]}")
+    for i in range(len(highest_res)):
+        table_leaders.append(f"{number[i][0]} - {nickname[i][0]} - {highest_res[i][0]}")
+
+    return table_leaders
